f81749e081d622f8d2ee5db1f8a981c0a7171111
Sean <chenxiaomeng1987@gmail.com>
2014-05-22 23:39:53 -0400

    1. Add ListNode structure 2. Add LinkedListCycle 1 and 2

---
+++ src/com/leetcode/core/ListNode.java
@@ -0,0 +1,13 @@
+package com.leetcode.core;
+
+/**
+ * Created by Xiaomeng on 5/22/2014.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val){
+        this.val = val;
+        next = null;
+    }
+}
