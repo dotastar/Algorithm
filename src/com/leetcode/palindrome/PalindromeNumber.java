e3811c5dfb206e2845ccadf9ba1a7b24fa424c3c
Sean <chenxiaomeng1987@gmail.com>
2014-08-17 01:02:19 -0400

    Solution for Palindrome Number.

---
+++ src/com/leetcode/palindrome/PalindromeNumber.java
@@ -0,0 +1,39 @@
+package com.leetcode.palindrome;
+
+/**
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ *
+ * Some hints:
+ *
+ * Could negative integers be palindromes? (ie, -1)
+ *
+ * If you are thinking of converting the integer to string, note the restriction of using extra space.
+ *
+ * You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+ *
+ * There is a more generic way of solving this problem.
+ *
+ * Created by Xiaomeng on 8/17/2014.
+ */
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+
+        int div = 1;
+        while(x / div >= 10) div *= 10;
+
+        while(x != 0){
+            int left = x / div;
+            int right = x % 10;
+            if(left != right) return false;
+            x = (x % div) / 10;
+            div /= 100;
+        }
+        return true;
+    }
+
+    public static void main(String[] args){
+        PalindromeNumber test = new PalindromeNumber();
+        System.out.println(test.isPalindrome(33133));
+    }
+}
