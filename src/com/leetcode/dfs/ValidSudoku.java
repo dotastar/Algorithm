9b38992535795aa2e13e81fa3aa20756be74f003
Sean <chenxiaomeng1987@gmail.com>
2014-08-25 02:09:17 -0400

    Solution for Valid Sudoku.

--- src/com/leetcode/dfs/ValidSudoku.java
+++ src/com/leetcode/dfs/ValidSudoku.java
@@ -4,60 +4,58 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Determine if a Sudoku is valid.
+ * Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
  *
  * The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
  *
  * Note:
  * A valid Sudoku board (partially filled) is not necessarily solvable. Only the filled cells need to be validated.
  *
- * Created by Xiaomeng on 5/18/2014.
+ * Created by Xiaomeng on 8/24/2014.
  */
 public class ValidSudoku {
-    public static boolean isValidSudoku(char[][] board) {
-        if(board.length != 9 || board[0].length != 9) return false;
-
+    public boolean isValidSudoku(char[][] board) {
         Set<Character> set = new HashSet<Character>();
+        //Row
         for(int i = 0; i < 9; i++){
             set.clear();
             for(int j = 0; j < 9; j++){
                 if(board[i][j] == '.') continue;
-                if(!Character.isDigit(board[i][j])) return false;
                 if(set.contains(board[i][j])) return false;
                 set.add(board[i][j]);
             }
         }
 
+        //Column
         for(int i = 0; i < 9; i++){
             set.clear();
             for(int j = 0; j < 9; j++){
                 if(board[j][i] == '.') continue;
-                if(!Character.isDigit(board[j][i])) return false;
                 if(set.contains(board[j][i])) return false;
                 set.add(board[j][i]);
             }
         }
 
-        int x = 0, y = 0;
-        while(y < 9){
+        int i = 0, j = 0;
+        while(i < 9 && j < 9){
             set.clear();
-            for(int i = x; i < x + 3; i++){
-                for(int j = y; j < y + 3; j++){
-                    if(board[i][j] == '.') continue;
-                    if(!Character.isDigit(board[i][j])) return false;
-                    if(set.contains(board[i][j])) return false;
-                    set.add(board[i][j]);
+            for(int m = i; m < i + 3; m++){
+                for(int n = j; n < j + 3; n++){
+                    if(board[m][n] == '.') continue;
+                    if(set.contains(board[m][n])) return false;
+                    set.add(board[m][n]);
                 }
             }
-            if(x == 6){
-                x = 0;
-                y += 3;
+            if(j == 6){
+                i += 3;
+                j = 0;
             }else{
-                x += 3;
+                j += 3;
             }
         }
         return true;
     }
+
 
     public static void main(String[] args){
         char[][] board = {
@@ -71,6 +69,8 @@ public class ValidSudoku {
                 { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
                 { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
         };
-        System.out.println(isValidSudoku(board));
+
+        ValidSudoku test = new ValidSudoku();
+        System.out.println(test.isValidSudoku(board));
     }
 }
