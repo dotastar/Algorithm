fed66330d788f71700c1074e9dca1fa960961953
Sean <chenxiaomeng1987@gmail.com>
2014-08-29 00:49:29 -0400

    Interval Class.

---
+++ src/com/leetcode/core/Interval.java
@@ -0,0 +1,19 @@
+package com.leetcode.core;
+
+/**
+ * Created by Xiaomeng on 8/21/2014.
+ */
+public class Interval {
+    public int start;
+    public int end;
+
+    public Interval(){
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+}
