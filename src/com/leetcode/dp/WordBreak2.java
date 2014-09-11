07d142352b6d51fe05a4f4275d6111c255457909
Sean <chenxiaomeng1987@gmail.com>
2014-09-10 23:35:52 -0400

    Solution for Word Break2.

---
+++ src/com/leetcode/dp/WordBreak2.java
@@ -0,0 +1,111 @@
+package com.leetcode.dp;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *  Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
+ *
+ *  Return all such possible sentences.
+ *
+ *  For example, given
+ *  s = "catsanddog",
+ *  dict = ["cat", "cats", "and", "sand", "dog"].
+ *
+ *  A solution is ["cats and dog", "cat sand dog"].
+ *
+ *  Created by Xiaomeng on 9/10/2014.
+ */
+public class WordBreak2 {
+    List<String> result;
+    StringBuilder single;
+
+    /*
+    *
+    * DFS Solution: TLE
+    *
+    * */
+    public List<String> wordBreak(String s, Set<String> dict) {
+        result = new ArrayList<String>();
+        single = new StringBuilder();
+        wordBreak(s, dict, 0);
+        return result;
+    }
+
+    public void wordBreak(String s, Set<String> dict, int index){
+        if(index ==  s.length()){
+            result.add(single.toString());
+            return;
+        }
+
+        for(String word : dict){
+            if(index + word.length() <= s.length()){
+                String sub = s.substring(index, index + word.length());
+                if(sub.equals(word)){
+                    single.append(word);
+                    if(index + word.length() != index) single.append(" ");
+                    wordBreak(s, dict, index + word.length());
+                    if(index + word.length() != index) single.deleteCharAt(single.length() - 1);
+                    single.delete(index, index + word.length());
+                }
+            }
+        }
+    }
+
+    /*
+    *
+    * DP Solution
+    *
+    * */
+    public List<String> wordBreak2(String s, Set<String> dict) {
+        result = new ArrayList<String>();
+        single = new StringBuilder();
+        List<List<Integer>> memo = new ArrayList<List<Integer>>();
+        for(int i = 0; i < s.length() + 1; i++){
+            memo.add(new ArrayList<Integer>());
+        }
+        for(int i = 0; i < s.length(); i++){
+            for(String word : dict){
+                int end = i + word.length();
+                if(end <= s.length()){
+                    String sub = s.substring(i, end);
+                    if(sub.equals(word)){
+                        memo.get(end).add(i);
+                    }
+                }
+            }
+        }
+        wordBreak2(s, s.length(), memo);
+        return result;
+    }
+
+    public void wordBreak2(String s, int curr, List<List<Integer>> memo){
+        if(curr == 0){
+            result.add(single.toString());
+            return;
+        }
+        for(int p : memo.get(curr)){
+            String sub = s.substring(p, curr);
+            if(curr != s.length()) single.insert(0, " ");
+            single.insert(0, sub);
+            wordBreak2(s, p, memo);
+            if(curr != s.length()) single.deleteCharAt(0);
+            single.delete(0, curr - p);
+
+        }
+    }
+
+    public static void main(String[] args){
+        WordBreak2 test = new WordBreak2();
+        String s = "catsanddog";
+        Set<String> dict = new HashSet<String>();
+        dict.add("cat");
+        dict.add("cats");
+        dict.add("and");
+        dict.add("sand");
+        dict.add("dog");
+        System.out.println(test.wordBreak2(s, dict));
+    }
+}
