3e570ec68a9e2b7fb7c7fd1356851335e921ab38
Sean <chenxiaomeng1987@gmail.com>
2014-10-16 02:00:33 -0400

    Refactor the code and add comments.

--- src/com/leetcode/dp/WordLadder2.java
+++ src/com/leetcode/dp/WordLadder2.java
@@ -34,6 +34,28 @@ public class WordLadder2 {
     Map<String, List<String>> map;
     List<String> single;
 
+    /**
+     *
+     * Sample:
+     *            tad
+     *          /     \
+     *       ted        tax
+     *     /    \     /
+     * red        tex
+     *     \    /
+     *       rex
+     *
+     * WordLadder1:
+     * Since we only need to calculate the shortest length, we don't care the case that both 'ted' and 'red' will be transformed to the same 'tex'.
+     * Hence, for performance purpose, we just remove 'tex' in dictionary when transforming 'ted' to 'tex',
+     * in which case, when considering 'rex', we don't even need to perform any operations give 'tex' is already removed from the dict.
+     *
+     * WordLadder2:
+     * Since we need to track down each shortest path, we can't remove the 'tex' when mapping 'ted' as we still need to track down 'rex';
+     * So, we remove both 'ted' and 'rex' in one shot in the next loop.
+     *
+     */
+
     public List<List<String>> findLadders(String start, String end, Set<String> dict) {
         result = new ArrayList<List<String>>();
         single = new ArrayList<String>();
@@ -59,7 +81,7 @@ public class WordLadder2 {
             currentLevel.clear();
             for(int i = 0; i < size; i++){
                 String top = queue.poll();
-                if(dict.contains(top)) dict.remove(top);
+                dict.remove(top);
                 currentLevel.add(top);
             }
 
@@ -109,12 +131,15 @@ public class WordLadder2 {
 
     public static void main(String[] args){
         WordLadder2 wordLadder = new WordLadder2();
-        String start = "hot";
-        String end = "dot";
+        String start = "red";
+        String end = "tax";
         HashSet<String> dict = new HashSet<String>();
-        dict.add("hot");
-        dict.add("dot");
-        dict.add("dog");
+        dict.add("ted");
+        dict.add("tex");
+        dict.add("red");
+        dict.add("tax");
+        dict.add("tad");
+        dict.add("rex");
 
         for(List<String> single : wordLadder.findLadders(start, end, dict)){
             System.out.println(single);
