a98256cef435942cf270e49169cf1a342ea6c11f
Sean <chenxiaomeng1987@gmail.com>
2014-10-17 02:20:50 -0400

    Update with clear solution.

--- src/com/leetcode/dp/MaximumProductSubarray.java
+++ src/com/leetcode/dp/MaximumProductSubarray.java
@@ -9,6 +9,25 @@ package com.leetcode.dp;
  *  Created by Xiaomeng on 9/24/2014.
  */
 public class MaximumProductSubarray {
+
+    /**
+     * Clearer solution: http://www.cnblogs.com/dengeven/p/3992459.html
+     *
+     */
+    public int maxProduct1(int[] A) {
+        int maxEnding = A[0];
+        int minEnding = A[0];
+        int max = A[0];
+        for(int i = 1; i < A.length; i++){
+            int tmpMax = maxEnding * A[i];
+            int tmpMin = minEnding * A[i];
+            maxEnding = Math.max(A[i], Math.max(tmpMax, tmpMin));
+            minEnding = Math.min(A[i], Math.min(tmpMax, tmpMin));
+            if(maxEnding > max) max = maxEnding;
+        }
+        return max;
+    }
+
     public int maxProduct(int[] A) {
         if(A.length == 1) return A[0];
         int currMax = A[0], currMin = A[0], max = A[0];
