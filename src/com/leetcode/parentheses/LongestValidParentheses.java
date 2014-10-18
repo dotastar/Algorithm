4b9b48e226306740b064efb527d0b24e7baa53de
Sean <chenxiaomeng1987@gmail.com>
2014-10-18 19:32:05 -0400

    Add DP solution.

--- src/com/leetcode/parentheses/LongestValidParentheses.java
+++ src/com/leetcode/parentheses/LongestValidParentheses.java
@@ -12,7 +12,11 @@ import java.util.Stack;
  * Created by Xiaomeng on 8/24/2014.
  */
 public class LongestValidParentheses {
-    public int longestValidParentheses(String s) {
+    /**
+     * Stack solution
+     *
+     */
+    public int longestValidParentheses1(String s) {
         int max = 0;
         Stack<Integer> stack = new Stack<Integer>();
         for(int i = 0; i < s.length(); i++){
@@ -31,9 +35,31 @@ public class LongestValidParentheses {
         return max;
     }
 
+    /**
+     * DP solution
+     * http://blog.csdn.net/abcbc/article/details/8826782
+     * http://www.cnblogs.com/huntfor/p/3886111.html
+     *
+     */
+    public int longestValidParentheses2(String s) {
+        int[] dp = new int[s.length()];
+        int max = 0;
+        for(int i = s.length() - 2; i >= 0; i--){
+            if(s.charAt(i) == '('){
+                int j = i + dp[i + 1] + 1;
+                if(j < s.length() && s.charAt(j) == ')'){
+                    dp[i] = dp[i + 1] + 2;
+                    if(j + 1 < s.length()) dp[i] += dp[j + 1];
+                }
+                max = Math.max(max, dp[i]);
+            }
+        }
+        return max;
+    }
+
     public static void main(String[] args){
         LongestValidParentheses test = new LongestValidParentheses();
         String s = "()(())";
-        System.out.println(test.longestValidParentheses(s));
+        System.out.println(test.longestValidParentheses1(s));
     }
 }
