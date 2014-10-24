15746113cf5a82ba2b12ec524bd01ff38992a449
Sean <chenxiaomeng1987@gmail.com>
2014-10-23 22:12:29 -0400

    Refactor the code.

--- src/com/leetcode/string/LongestCommonPrefix.java
+++ src/com/leetcode/string/LongestCommonPrefix.java
@@ -8,14 +8,15 @@ package com.leetcode.string;
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         if(strs.length == 0) return "";
-        int i = 0;
-        for(; i < strs[0].length(); i++){
-            char c = strs[0].charAt(i);
-            for(int j = 1;j < strs.length; j++){
-                if(i >= strs[j].length() || strs[j].charAt(i) != c) return strs[0].substring(0,i);
+        if(strs.length == 1) return strs[0];
+
+        for(int i = 0; i < strs[0].length(); i++){
+            Character ch = strs[0].charAt(i);
+            for(int j = 1; j < strs.length; j++){
+                if(i >= strs[j].length() || strs[j].charAt(i) != ch) return strs[0].substring(0, i);
             }
         }
-        return strs[0].substring(0,i);
+        return strs[0].substring(0);
     }
 
     public static void main(String[] args){
