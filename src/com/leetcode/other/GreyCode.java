cbd62b9d11640a52695c6e9988b6f97c916093e5
Sean <chenxiaomeng1987@gmail.com>
2014-11-01 17:53:40 -0400

    Update with comments.

--- src/com/leetcode/other/GreyCode.java
+++ src/com/leetcode/other/GreyCode.java
@@ -30,9 +30,22 @@ public class GreyCode {
      * http://yucoding.blogspot.com/2013/01/leetcode-question-33-gary-code.html
      * http://www.cnblogs.com/TenosDoIt/p/3451938.html
      * http://www.cnblogs.com/lihaozy/archive/2012/12/31/2840437.html
+     * http://www.matrix67.com/blog/archives/266 (Very intuitive!)
      *
-     * @param n
-     * @return List of Integer
+     * 二进制数   Gray码
+     * 000       000
+     * 001       001
+     * 010       011
+     * 011       010
+     * 100       110
+     * 101       111
+     * 110       101
+     * 111       100
+     *
+     * 从二进制数的角度看，“镜像”位置上的数即是对原数进行not运算后的结果。比如，第3个数010和倒数第3个数101的每一位都正好相反。
+     * 假设这两个数分别为x和y，那么x xor (x shr 1)和y xor (y shr 1)的结果只有一点不同：后者的首位是1，前者的首位是0。
+     * 而这正好是Gray码的生成方法。这就说明了，Gray码的第n个数确实是n xor (n shr 1)。
+     *
      */
     public List<Integer> grayCode(int n) {
         List<Integer> result = new ArrayList<Integer>();
