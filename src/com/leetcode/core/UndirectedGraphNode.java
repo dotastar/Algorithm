5fd26195187ecf4e679bf71ca9c076742978168e
Sean <chenxiaomeng1987@gmail.com>
2014-11-11 03:29:59 -0500

    Update.

--- src/com/leetcode/core/UndirectedGraphNode.java
+++ src/com/leetcode/core/UndirectedGraphNode.java
@@ -9,9 +9,11 @@ import java.util.List;
 public class UndirectedGraphNode {
     public int label;
     public List<UndirectedGraphNode> neighbors;
+    public boolean visited;
 
     public UndirectedGraphNode(int x) {
         label = x;
         neighbors = new ArrayList<UndirectedGraphNode>();
+        visited = false;
     }
 }
