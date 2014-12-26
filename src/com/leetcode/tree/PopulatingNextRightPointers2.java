1c1ade2a352f71500717d26e356d08fa0648153e
Sean <chenxiaomeng1987@gmail.com>
2014-12-26 18:11:52 -0500

    Refactor the code and add comments

--- src/com/leetcode/tree/PopulatingNextRightPointers2.java
+++ src/com/leetcode/tree/PopulatingNextRightPointers2.java
@@ -31,19 +31,29 @@ import com.leetcode.core.TreeLinkNode;
  * Created by Xiaomeng on 8/11/2014.
  */
 public class PopulatingNextRightPointers2 {
+    /*
+    * Why recursive the right subtree first?
+    *
+    * Think about what is happening in this case:
+    *
+    *         1
+    *       /  \
+    *      2    3
+    *     /    / \
+    *    4    5   7
+    *     \      / \
+    *     10    11 12
+    *
+    * */
     public void connect(TreeLinkNode root) {
         if(root == null) return;
 
         TreeLinkNode node = root.next;
-        while(node != null){
-            if(node.left == null && node.right == null)
-                node = node.next;
-            else
-                break;
+        while(node != null && node.left == null && node.right == null){
+            node = node.next;
         }
 
-        TreeLinkNode next = node == null ? null :
-                            node.left != null ? node.left : node.right;
+        TreeLinkNode next = node == null ? null : node.left != null ? node.left : node.right;
 
         if(root.left != null){
             root.left.next = root.right != null ? root.right : next;
