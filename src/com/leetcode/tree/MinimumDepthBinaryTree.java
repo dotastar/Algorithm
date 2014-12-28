2291b3b7f2c01a8e358700f999e95c083fbbb6ed
Sean <chenxiaomeng1987@gmail.com>
2014-12-27 20:10:03 -0500

    Add level-first traversal solution.

--- src/com/leetcode/tree/MinimumDepthBinaryTree.java
+++ src/com/leetcode/tree/MinimumDepthBinaryTree.java
@@ -2,6 +2,9 @@ package com.leetcode.tree;
 
 import com.leetcode.core.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Given a binary tree, find its minimum depth.
  *
@@ -10,11 +13,38 @@ import com.leetcode.core.TreeNode;
  * Created by Xiaomeng on 8/11/2014.
  */
 public class MinimumDepthBinaryTree {
+    /**
+     * DFS
+     */
     public int minDepth(TreeNode root) {
         if(root == null) return 0;
         if(root.left == null) return minDepth(root.right) + 1;
         if(root.right == null) return minDepth(root.left) + 1;
         return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+
+    /**
+     * Level-first traversal
+     *
+     */
+    public int minDepth2(TreeNode root) {
+        if(root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        int level = 1;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode node = queue.poll();
+                if(node.left == null && node.right == null){
+                    return level;
+                }
+                if(node.left != null) queue.add(node.left);
+                if(node.right != null) queue.add(node.right);
+            }
+            level++;
+        }
+        return 0;
     }
 
     public static void main(String[] args){
