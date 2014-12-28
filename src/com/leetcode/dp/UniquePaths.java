58350bdf1d31efdd9d63bb9362ac20fce34d6f45
Sean <chenxiaomeng1987@gmail.com>
2014-12-27 22:39:46 -0500

    Add different solutions.

--- src/com/leetcode/dp/UniquePaths.java
+++ src/com/leetcode/dp/UniquePaths.java
@@ -12,28 +12,82 @@ package com.leetcode.dp;
  * Created by Xiaomeng on 8/18/2014.
  */
 public class UniquePaths {
-    public int uniquePaths(int m, int n) {
-        int[][] memo = new int[m][n];
-        for(int i = 0; i < memo.length; i++){
-            for(int j = 0; j < memo[0].length; j++){
-                memo[i][j] = -1;
-            }
-        }
-        memo[1][1] = 1;
-        return uniquePaths(m, n, memo);
+    /**
+     * Recursive: 2^n
+     *
+     */
+    public int uniquePaths1(int m, int n) {
+        if(m == 1 || n == 1) return 1;
+        return uniquePaths1(m - 1, n) + uniquePaths1(m, n - 1);
     }
 
-    public int uniquePaths(int m, int n, int[][] memo){
-        if(m < 1 || n < 1 || m >= memo.length || n >= memo[0].length) return 0;
 
-        if(memo[m][n] == -1){
-            memo[m][n] = uniquePaths(m - 1 , n, memo) + uniquePaths(m, n - 1, memo);
+    /**
+     * Bottom-up DP
+     * Time: O(mn)
+     * Space: O(mn)
+     *
+     */
+    public int uniquePaths2(int m, int n) {
+        int[][] memo = new int[m + 1][n + 1];
+        for(int i = 1; i < m + 1; i++){
+            for(int j = 1; j < n + 1; j++){
+                if(i == 1)
+                    memo[i][j] = 1;
+                else if(j == 1)
+                    memo[i][j] = 1;
+                else{
+                    memo[i][j] = memo[i - 1][j] + memo[i][j - 1];
+                }
+            }
+        }
+        return memo[m][n];
+    }
+
+    /**
+     * Bottom-up DP
+     * Time: O(mn)
+     * Space: O(n)
+     *
+     */
+    public int uniquePaths3(int m, int n) {
+        int[] curr = new int[n + 1];
+        int[] next = new int[n + 1];
+        for(int i = 1; i < n + 1; i++) curr[i] = 1;
+
+        for(int i = 2; i < m + 1; i++){
+            for(int j = 1; j < n + 1; j++){
+                if(j == 1){
+                    next[j] = 1;
+                }else{
+                    next[j] = next[j - 1] + curr[j];
+                }
+            }
+            curr = next;
+        }
+        return curr[n];
+    }
+
+    /**
+     * Top-down DP
+     * Time: O(mn)
+     * Space: O(mn)
+     */
+    public int uniquePaths4(int m, int n) {
+        int[][] memo = new int[m + 1][n + 1];
+        for(int i = 1; i < m + 1; i++) memo[i][1] = 1;
+        for(int i = 1; i < n + 1; i++) memo[1][i] = 1;
+        return uniquePaths4(m, n, memo);
+    }
+
+    public int uniquePaths4(int m, int n, int[][] memo){
+        if(memo[m][n] == 0){
+            memo[m][n] = uniquePaths4(m - 1, n, memo) + uniquePaths4(m, n - 1, memo);
         }
         return memo[m][n];
     }
 
     public static void main(String[] args){
-        UniquePaths test = new UniquePaths();
-        System.out.println(test.uniquePaths(2, 3));
+
     }
 }
